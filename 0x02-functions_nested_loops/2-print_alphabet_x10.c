91d8e722421265d027bb60431aeb33a1895616d9
neryomollo <neryomollo@gmail.com>
2023-09-12 21:11:33 +0300

    changed code for question 2 alphabet x10 task in c funtions and nested loops project

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,21 +3,15 @@
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	while (count <= 9)
+	char letter;
 
+	while (count++ <= 9)
 	{
-		char letter = 'a';
-		while (letter <= 'z')
-
-		{
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-			letter++;
-		}
 		_putchar('\n');
-		count++;
 	}
 }
