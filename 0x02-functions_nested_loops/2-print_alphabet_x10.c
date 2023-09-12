932a3477c6d0321804415fba5208b2fd88bbacc6
neryomollo <neryomollo@gmail.com>
2023-09-12 21:01:39 +0300

    revisions to question 2 (10x alphabet) to fic betty compliance

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
 
 void print_alphabet_x10(void)
 {
-  int count = 0;
-  while(count <= 9)
-  {
-  char letter = 'a';
-  while(letter <= 'z')
-  {
-    _putchar(letter);
-    letter++;
-  }
-  _putchar('\n');
-  count++;
-  }
+	int count = 0;
+	while (count <= 9)
+
+	{
+		char letter = 'a';
+		while (letter <= 'z')
+
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		count++;
+	}
 }
